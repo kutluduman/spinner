f20689333a7a7c01153a4e2030c5ecfe08a27a28
Kutluhan Duman <kutluduman@gmail.com>
2020-07-28 19:31:54 +0000

    Refactored with Eslint

--- spinner2.js
+++ spinner2.js
@@ -3,7 +3,7 @@ let symbolsArr =  (['\r|  ', '\r/  ', '\r-  ', '\r\\   ', '\r|  ', '\r/  ', '\r-
 let time = 0;
 let incremental = 150;
 
-for(const symbol in symbolsArr) {
+for (const symbol in symbolsArr) {
   setTimeout(() => {
     process.stdout.write(symbolsArr[symbol]);
   },time += incremental);
